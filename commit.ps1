58f008c05c45eb5e5a5ea0d564d0835901f32891
shashidhar <s94094838@gmail.com>
2026-02-02 13:48:25 +0530

    Delete commit.ps1

--- commit.ps1
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env pwsh
-cd "c:\Users\s9409\Downloads\portfolio2"
-git add index.html
-git commit -m "Fix 40 errors: add rel='noopener' to links, add webkit prefixes, remove invalid space-y CSS"
-git push origin main
